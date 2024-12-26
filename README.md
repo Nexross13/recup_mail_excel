9a93e5aeefb5d31838a5e021b27e3216dd57cc25
Lilian BROUCA <lilianbrouca1302@gmail.com>
2024-12-26 18:05:22 +0100

    Colonne devient une variable (plus facile à changer)

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # recup_mail_excel
+
+Ce programme lit l'export de vos commandes HelloAsso (en fichier Excel) , extrait les données de la colonne spécifique des adresses mail (généralement F), supprime les doublons, et écrit les résultats dans un fichier texte.
+
+## Prérequis
+
+- Python 3.11 ou supérieur
+- Les bibliothèques Python suivantes :
+  - `pandas`
+  - `openpyxl`
+
+Vous pouvez installer les dépendances avec la commande suivante :
+
+```bash
+pip install pandas openpyxl
+```
+
+## Instructions d'Utilisation
+1. Placez votre fichier Excel dans le même répertoire que le script.
+2. Modifiez les variables fichier_excel et fichier_sortie dans le script :
+- fichier_excel : Chemin vers votre fichier Excel.
+- fichier_sortie : Nom ou chemin du fichier texte de sortie.
+3. Exécutez le script avec la commande suivante :
+```bash
+python3 recup_mail_excel.py
+```
+4. Le fichier de sortie contiendra toutes les adresses e-mail uniques.
+
+## Dépannage
+
+- Erreur : "Missing optional dependency 'openpyxl'" :
+Installez openpyxl avec la commande :
+```bash
+pip install openpyxl
+```
+- Erreur : "Fichier introuvable" :
+Assurez-vous que le chemin du fichier Excel est correct.
+
+- La colonne F n'existe pas :
+Vérifiez que la colonne F existe dans votre fichier Excel et contient les données à extraire.
--- recup_mail_excel.py
+++ recup_mail_excel.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-def lire_colonne_f(fichier_excel, fichier_sortie):
+def lire_colonne_f(fichier_excel, fichier_sortie, colonne):
     try:
         # Lecture du fichier Excel
         donnees = pd.read_excel(fichier_excel, engine='openpyxl')
         
         # Vérification si la colonne F existe
-        if 'F' in donnees.columns or donnees.shape[1] >= 6:
+        if colonne in donnees.columns or donnees.shape[1] >= (ord('F') - 64):
             # Récupération de la colonne F (en index 5 si c'est la 6e colonne)
             colonne_f = donnees.iloc[:, 5]
             
@@ -20,13 +20,14 @@ def lire_colonne_f(fichier_excel, fichier_sortie):
             
             print(f"Les emails uniques ont été écrits dans {fichier_sortie}.")
         else:
-            print("La colonne F n'existe pas dans le fichier fourni.")
+            print(f"La colonne {colonne} n'existe pas dans le fichier fourni.")
     except FileNotFoundError:
         print("Le fichier spécifié est introuvable.")
     except Exception as e:
         print(f"Une erreur s'est produite : {e}")
 
 # Exemple d'utilisation
-fichier_excel = ".xlsx"  # Remplacez par le chemin de votre fichier Excel
+fichier_excel = "merch.xlsx"  # Remplacez par le chemin de votre fichier Excel
 fichier_sortie = "emails_uniques.txt"  # Nom du fichier de sortie
-lire_colonne_f(fichier_excel, fichier_sortie)
+colonne = 'F'
+lire_colonne_f(fichier_excel, fichier_sortie, colonne)
